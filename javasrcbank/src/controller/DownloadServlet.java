f3323127f89ad0de1843bc88e655ebc3e45373dd
krishnapadala <krishnapadala@sg01-dzfqzf2.mhf.mhc>
2019-12-08 15:09:25 +0530

    download contro

---
+++ javasrcbank/src/controller/DownloadServlet.java
@@ -0,0 +1,58 @@
+package controller;
+
+import java.io.FileInputStream;
+import java.io.IOException;
+import java.io.PrintWriter;
+
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+/**
+ * Servlet implementation class DownloadServlet
+ */
+public class DownloadServlet extends HttpServlet {
+	private static final long serialVersionUID = 1L;
+       
+    /**
+     * @see HttpServlet#HttpServlet()
+     */
+    public DownloadServlet() {
+        super();
+        // TODO Auto-generated constructor stub
+    }
+
+	/**
+	 * @see HttpServlet#doGet(HttpServletRequest request, HttpServletResponse response)
+	 */
+	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		// TODO Auto-generated method stub
+		response.getWriter().append("Served at: ").append(request.getContextPath());
+	}
+
+	/**
+	 * @see HttpServlet#doPost(HttpServletRequest request, HttpServletResponse response)
+	 */
+	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		// TODO Auto-generated method stub
+		doGet(request, response);
+		
+		response.setContentType("text/html");  
+		PrintWriter out = response.getWriter();  
+		String filename = "home.jsp";   
+		String filepath = "e:\\";   
+		response.setContentType("APPLICATION/OCTET-STREAM");   
+		response.setHeader("Content-Disposition","attachment; filename=\"" + filename + "\"");   
+		  
+		FileInputStream fileInputStream = new FileInputStream(filepath + filename);  
+		            
+		int i;   
+		while ((i=fileInputStream.read()) != -1) {  
+		out.write(i);   
+		}   
+		fileInputStream.close();   
+		out.close();   
+		}  
+	}
+
